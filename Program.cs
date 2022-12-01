bad2dcd67bfb53e9bcbf552a811c3c55efec0610
dynamics <31436988+dynamics1@users.noreply.github.com>
2022-12-01 14:39:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		Console.Write("Introduceti cate numere va contine sirul:");
+		Console.Write("\n");
+		Console.Write("n= ");
+
+
+		int n;
+		int pare = 0;
+		n = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("\n");
+
+		for (int i = 0; i < n; i++)
+		{
+			int nr;
+			nr = Convert.ToInt32(Console.ReadLine());
+			if (nr % 2 == 0)
+			{
+				pare++;
+			}
+			
+		}
+		Console.Write("\n");
+		Console.Write("Sirul contine "+pare+" numere pare");
+		Console.Write("\n");
+
+
+	}
+}
